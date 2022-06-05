30e0a19bade614a161d2b7eee967e24bbe2a3359
Ian Kibet <ian.kibet@primelab.io>
2022-06-05 19:29:05 +0300

    initial

---
+++ lib/firecracker/client_transports_test.go
@@ -0,0 +1,80 @@
+// Copyright Amazon.com, Inc. or its affiliates. All Rights Reserved.
+//
+// Licensed under the Apache License, Version 2.0 (the "License"). You may
+// not use this file except in compliance with the License. A copy of the
+// License is located at
+//
+//	http://aws.amazon.com/apache2.0/
+//
+// or in the "license" file accompanying this file. This file is distributed
+// on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+// express or implied. See the License for the specific language governing
+// permissions and limitations under the License.
+
+package firecracker
+
+import (
+	"context"
+	"net"
+	"net/http"
+	"os"
+	"testing"
+	"time"
+
+<<<<<<< HEAD
+	"titan/lib/firecracker/client/operations"
+
+=======
+	"github.com/Ian-Kibet/firecracker-go-sdk/client/operations"
+>>>>>>> b8aa219df3977843c18fb0ce7af8af072b1bf0b8
+	"github.com/go-openapi/runtime"
+	"github.com/stretchr/testify/assert"
+)
+
+const expectedEndpointPath = "/test-operation"
+
+func TestNewUnixSocketTransport(t *testing.T) {
+	done := make(chan bool)
+
+	socketPath := "testingUnixSocket.sock"
+	listener, err := net.Listen("unix", socketPath)
+	if err != nil {
+		panic(err)
+	}
+
+	server := http.Server{
+		Handler: http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+			assert.Equal(t, expectedEndpointPath, req.URL.String())
+			w.WriteHeader(http.StatusOK)
+			close(done)
+		}),
+	}
+
+	go server.Serve(listener)
+	defer func() {
+		server.Shutdown(context.Background())
+		listener.Close()
+		os.Remove(socketPath)
+	}()
+
+	unixTransport := NewUnixSocketTransport(socketPath, nil, false)
+	unixTransport.Submit(testOperation())
+
+	select {
+	case <-time.After(100 * time.Millisecond):
+		t.Errorf("Timed out from the listener")
+	case <-done:
+	}
+}
+
+func testOperation() *runtime.ClientOperation {
+	return &runtime.ClientOperation{
+		ID:                 "putLogger",
+		Method:             "PUT",
+		PathPattern:        expectedEndpointPath,
+		ProducesMediaTypes: []string{"application/json"},
+		Schemes:            []string{"http"},
+		Params:             operations.NewPutLoggerParams(),
+		Reader:             &operations.PutLoggerReader{},
+	}
+}
---
+++ lib/firecracker/go.mod
@@ -0,0 +1,51 @@
+module github.com/Ian-Kibet/firecracker-go-sdk
+
+go 1.18
+
+require (
+	github.com/containerd/fifo v1.0.0
+	github.com/containernetworking/cni v1.1.1
+	github.com/containernetworking/plugins v1.1.1
+	github.com/go-openapi/errors v0.20.2
+	github.com/go-openapi/runtime v0.24.1
+	github.com/go-openapi/strfmt v0.21.2
+	github.com/go-openapi/swag v0.21.1
+	github.com/go-openapi/validate v0.21.0
+	github.com/go-ping/ping v1.1.0
+	github.com/google/uuid v1.3.0
+	github.com/hashicorp/go-multierror v1.1.1
+	github.com/mdlayher/vsock v1.1.1
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.7.1
+	github.com/vishvananda/netlink v1.1.1-0.20210330154013-f5de75959ad5
+	golang.org/x/sys v0.0.0-20220520151302-bc2c85ada10a
+)
+
+require (
+	github.com/PuerkitoBio/purell v1.1.1 // indirect
+	github.com/PuerkitoBio/urlesc v0.0.0-20170810143723-de5bf2ad4578 // indirect
+	github.com/asaskevich/govalidator v0.0.0-20210307081110-f21760c49a8d // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/go-openapi/analysis v0.21.2 // indirect
+	github.com/go-openapi/jsonpointer v0.19.5 // indirect
+	github.com/go-openapi/jsonreference v0.19.6 // indirect
+	github.com/go-openapi/loads v0.21.1 // indirect
+	github.com/go-openapi/spec v0.20.4 // indirect
+	github.com/go-stack/stack v1.8.1 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
+	github.com/mdlayher/socket v0.2.0 // indirect
+	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/oklog/ulid v1.3.1 // indirect
+	github.com/opentracing/opentracing-go v1.2.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/vishvananda/netns v0.0.0-20210104183010-2eb08e3e575f // indirect
+	go.mongodb.org/mongo-driver v1.8.3 // indirect
+	golang.org/x/net v0.0.0-20220127200216-cd36cc0744dd // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
+	golang.org/x/text v0.3.7 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+)
